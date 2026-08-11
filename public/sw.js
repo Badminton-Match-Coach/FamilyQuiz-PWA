bae2a9f6d66ceaba261fd8e7d670c16ae4ca2a9c
Bo-Göran Luttrén <bg.luttren@gmail.com>
2026-08-11 12:55:45 +0200

    20260811_1255

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'familyquiz-pwa-v1';
+const CACHE_NAME = 'FamilyQuiz-PWA-v1';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
